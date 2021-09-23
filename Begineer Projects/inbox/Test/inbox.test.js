e05a96d40f8388480d0203edda27693ee55737ef
Atharva Vijay Khade <atharvavk123@gmail.com>
2021-09-23 20:35:19 +0530

    Mocha Testing added

--- Begineer Projects/inbox/Test/inbox.test.js
+++ Begineer Projects/inbox/Test/inbox.test.js
@@ -2,3 +2,30 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
+const web3 = new Web3(ganache.provider());
+
+// testing using mocha 
+
+class Car {
+    drive(){
+        return "driving";
+    }
+    stop(){
+        return "stopping";
+    }
+}
+
+let car;
+
+beforeEach(()=>{
+    car = new Car();
+});
+
+describe('Testing it statements',()=>{
+    it('in it1 : testing drive',()=>{
+        assert.equal(car.drive(),"driving")
+    });
+    it('in it2 : testing stop',()=>{
+        assert.equal(car.stop(),'stopping');
+    });
+});
